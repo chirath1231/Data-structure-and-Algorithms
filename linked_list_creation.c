f8c1063ce6a3ffb7a30d0fd46ebd34237763fceb
chirath1231 <chirathhettiarachchi761@gmail.com>
2026-02-07 13:52:31 +0530

    creation

---
+++ linked_list_creation.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+	int data;
+	struct node * next;	
+};
+
+int main(){
+	struct node* head = (struct node*)malloc(sizeof(struct node));
+	struct node* second = (struct node*)malloc(sizeof(struct node));
+	struct node* third = (struct node*)malloc(sizeof(struct node));
+	head->data=10;
+	head->next=second;
+	second->data=20;
+	second->next=third;
+	third->data=30;
+	third->next=NULL;
+	
+	
+	struct node* ptr=head;
+
+	while(ptr!=NULL){
+		printf("%d->",ptr->data);
+		ptr=ptr->next;	
+	}
+	
+		printf("NULL");
+
+	
+	
+	return 0;
+}
